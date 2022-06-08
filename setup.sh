8f4f6260699de9dbb872c0c94f1f869a7f1418d6
Viacheslav Zhuravel <99031872+PackmanDude@users.noreply.github.com>
2022-06-08 19:32:57 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,20 +25,32 @@ chooseWhatToDo()
 	case $software in
 		0)  echo "Abort..."; exit;;
 		1)  install("bash");;
-		2)  install("i3+status");;
+		2)  install("i3");;
 		4)  install("mpv");;
 		8)  install("nano");;
-		16) install("sway+waybar");;
+		16) install("sway");;
 		32) install("xfce4-terminal");;
 		#--#--#--#--#-###-#--#--#--#--#
-		3) install("bash", "i3+status");;
-		5) install("mpv", "bash");;
-		6) install("mpv", "i3+status");;
-		7) install("mpv", "bash", "i3+status");;
-		9) install("nano", "bash");;
-		10) install("nano", "i3+status");;
-		11) install("nano", "bash", "i3+status");;
-
+		3)  install("bash", "i3");;
+		5)  install("mpv", "bash");;
+		6)  install("mpv", "i3");;
+		7)  install("mpv", "bash", "i3");;
+		9)  install("nano", "bash");;
+		10) install("nano", "i3");;
+		11) install("nano", "bash", "i3");;
+		12) install("nano", "mpv");;
+		13) install("nano", "mpv", "bash");;
+		14) install("nano", "mpv", "i3");;
+		15) install("nano", "mpv", "bash", "i3");;
+		17) install("sway", "bash");;
+		18) install("sway", "i3");;	# What reason you have for that
+		19) install("sway", "bash", "i3");;
+		20) install("sway", "mpv");;
+		21) install("sway", "mpv", "bash");;
+		22) install("sway", "mpv", "i3");;
+		23) install("sway", "mpv", "bash", "i3");;
+		24) install("sway", "bash");;
+		25) install("sway", "i3");;
 
 		*) echo "Not impelemented answer, aborting"; exit 1;;
 	esac
